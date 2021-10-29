07abf4fe0a8a5623fb850f3f438b8c2caff827b7
Megan Kuo <megan.kuo@live.com>
2021-10-28 17:31:48 -0700

    initial commit

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace Assignment1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
